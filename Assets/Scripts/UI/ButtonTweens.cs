e55a674f0030bf868d6c66c754e69fd6ab7118ac
David Morris <pixelgrapher@outlook.co.nz>
2021-05-05 17:16:37 +1200

    Implement play button effects

--- Assets/Scripts/UI/ButtonTweens.cs
+++ Assets/Scripts/UI/ButtonTweens.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using DG.Tweening;
 
@@ -13,12 +15,17 @@ public class ButtonTweens : MonoBehaviour, IPointerDownHandler, IPointerUpHandle
     [SerializeField, Tooltip("Duration of tween when pointer is released (e.g 0.1)")]
     private float m_tweenOutDuration = 0.1f;
 
+    [SerializeField, Tooltip("Ring that radiates outward when button is clicked")]
+    private CanvasGroup ring;
+
     [SerializeField, Tooltip("Prevent tween from playing if locked")]
 	private bool m_locked = false;
 
     private Vector2 m_originalScale;
     private Tween m_tween = null;
-    private Tween m_tween2 = null;
+
+    private Tween[] m_clickTweens;
+    private bool m_clickTweensInitialized;
 
     private Sequence m_sequenceTemplate = null;
     private Sequence m_playingSequence = null;
@@ -26,6 +33,7 @@ public class ButtonTweens : MonoBehaviour, IPointerDownHandler, IPointerUpHandle
     private void Awake() 
     {
         m_originalScale = transform.localScale;
+        m_clickTweens = new Tween[3];
     }
 
     public void OnPointerDown(PointerEventData eventData)
@@ -35,8 +43,8 @@ public class ButtonTweens : MonoBehaviour, IPointerDownHandler, IPointerUpHandle
 			return;
 		}
         // Kill the current tween before running the new one
-        //m_tween?.Kill(true);
-        //m_tween = transform.DOScale(m_pressedScale * m_originalScale, m_tweenInDuration);
+        m_tween?.Kill(true);
+        m_tween = transform.DOScale(m_pressedScale * m_originalScale, m_tweenInDuration);
     }
 
     public void OnPointerUp(PointerEventData eventData)
@@ -46,14 +54,24 @@ public class ButtonTweens : MonoBehaviour, IPointerDownHandler, IPointerUpHandle
 			return;
 		}
         // Kill the current tween before running the new one
-        //m_tween?.Kill(true);
-        //m_tween = transform.DOScale(m_originalScale, m_tweenOutDuration);
+        m_tween?.Kill(true);
+        m_tween = transform.DOScale(m_originalScale, m_tweenOutDuration);
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        m_tween2?.Kill(true);
-        m_tween2 = transform.DOPunchRotation(Vector3.one * 10.0f, 0.5f);
+
+        for (int i = 0; i < m_clickTweens.Length; i++)
+        {
+            m_clickTweens[i].Kill(true);
+        }
+
+        ring.alpha = 0.5f;
+        ring.transform.localScale = Vector3.one;
+
+        m_clickTweens[0] = transform.DOPunchRotation(Vector3.one * 15.0f, 0.3f);
+        m_clickTweens[1] = ring.transform.DOScale(Vector3.one * 2.0f, 0.3f);
+        m_clickTweens[2] = ring.DOFade(0.0f, 0.3f);
 
     }
 
